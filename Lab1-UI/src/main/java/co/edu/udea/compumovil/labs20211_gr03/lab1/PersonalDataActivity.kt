25e5d427452f00ada8836c87865650227a78fd78
Doly Jimenez <doly.jimenez11@gmail.com>
2021-04-14 18:16:41 -0500

    Fields name, lastname and gender from personal information form

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.labs20211_gr03.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+    }
+}
