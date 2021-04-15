5dba7559256e53433a7e62aae80f335469f56260
Doly Jimenez <doly.jimenez11@gmail.com>
2021-04-15 00:15:05 -0500

    form version portrait

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
@@ -1,11 +1,24 @@
 package co.edu.udea.compumovil.labs20211_gr03.lab1
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+
+        //Spinner Grado de Escolaridad
+        val spinner = findViewById<Spinner>(R.id.spinner_schoolGrade)
+        var list = resources.getStringArray(R.array.array_gradoEscolaridad)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
+        spinner.adapter = adaptador
+
     }
 }
