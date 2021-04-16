70d47ba9fbd5eef96d1ebc3403f9ffab16358b19
Doly Jimenez <doly.jimenez11@gmail.com>
2021-04-16 17:31:00 -0500

    Change editText to Button for showing DataPicker

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
@@ -1,45 +1,42 @@
 package co.edu.udea.compumovil.labs20211_gr03.lab1
 
 import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import co.edu.udea.compumovil.labs20211_gr03.lab1.dialog.DatePickerFragment
+import java.util.*
 
 
 class PersonalDataActivity : AppCompatActivity() {
-    private lateinit var birthDate: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
-
-        birthDate = findViewById<EditText>(R.id.input_birthDate)
-        birthDate.setOnClickListener {
-            showDatePickerDialog()
-        }
-
 
         //Spinner Grado de Escolaridad
         val spinner = findViewById<Spinner>(R.id.spinner_schoolGrade)
         var list = resources.getStringArray(R.array.array_gradoEscolaridad)
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
         spinner.adapter = adaptador
-
-
     }
 
-    private fun showDatePickerDialog() {
-        val newFragment = DatePickerFragment.newInstance(DatePickerDialog.OnDateSetListener { _, year, month, day ->
-            // +1 because January is zero
-            val selectedDate = day.toString() + " / " + (month + 1) + " / " + year
-            birthDate.setText(selectedDate)
-        })
+    //Metodo para mostrar el DataPicker
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickDataPicker(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
 
-        newFragment.show(supportFragmentManager, "datePicker")
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            // Display Selected date in Toast
+            Toast.makeText(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show()
+        }, year, month, day)
+        dpd.show()
     }
-
-
 }
