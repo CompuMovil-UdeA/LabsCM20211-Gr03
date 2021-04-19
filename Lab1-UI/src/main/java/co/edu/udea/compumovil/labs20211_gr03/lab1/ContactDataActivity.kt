9867fd0b4dc358029d3d26745d56502d0eb06ff6
Antonio Gonzalez <antonio.gonzalezr@udea.edu.co>
2021-04-18 22:08:17 -0500

    Inputs Activity de Contacto con sus constraints

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/ContactDataActivity.kt
@@ -3,9 +3,9 @@ package co.edu.udea.compumovil.labs20211_gr03.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ContactDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_contact_data)
     }
 }
