9ca786fd5da0fdec0206a14cd7ebdde9796d4afd
Doly Jimenez <doly.jimenez11@gmail.com>
2021-04-21 23:22:19 -0500

    mostrar logcat activity informacion personal

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab1/PersonalDataActivity.kt
@@ -4,13 +4,11 @@ import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
@@ -25,6 +23,13 @@ class PersonalDataActivity : AppCompatActivity() {
     private lateinit var txt_lastnames: TextInputLayout
     private lateinit var input_lastnames: TextInputEditText
     private lateinit var btn_next: Button
+    private lateinit var fecha_nacimiento: String
+    private lateinit var rg_genero: RadioGroup
+    private lateinit var rta_genero: String
+    private lateinit var spinner: Spinner
+    private lateinit var btn_datapicker: Button
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,15 +39,22 @@ class PersonalDataActivity : AppCompatActivity() {
         input_names = findViewById(R.id.input_names)
         txt_lastnames = findViewById(R.id.txt_lastnames)
         input_lastnames = findViewById(R.id.input_lastnames)
+        btn_datapicker = findViewById(R.id.btn_showDatapicker)
         btn_next = findViewById(R.id.btn_next_infoContact)
-
-
+        fecha_nacimiento= ""
+        rg_genero = findViewById(R.id.rg_gender)
+        rta_genero = ""
 
         //Spinner Grado de Escolaridad
-        val spinner = findViewById<Spinner>(R.id.spinner_schoolGrade)
+        spinner = findViewById<Spinner>(R.id.spinner_schoolGrade)
         var list = resources.getStringArray(R.array.array_gradoEscolaridad)
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
         spinner.adapter = adaptador
+
+        val genero_por_defecto = findViewById<RadioButton>(R.id.rb_man)
+        genero_por_defecto.isSelected = true
+
+
 
 
         btn_next.setOnClickListener { v ->
@@ -51,8 +63,7 @@ class PersonalDataActivity : AppCompatActivity() {
 
             // No hay error
             if(txt_names.error == null && txt_lastnames.error == null ){
-                // Loggee
-
+                imprimirEnConsola()
 
                 val intent = Intent(this, ContactDataActivity::class.java)
                 startActivity(intent)
@@ -61,6 +72,28 @@ class PersonalDataActivity : AppCompatActivity() {
 
         validarCampos()
     }
+
+    //Metodo para mostrar el DataPicker
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickDataPicker(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                fecha_nacimiento = """$dayOfMonth - ${monthOfYear + 1} - $year"""
+                Toast.makeText(this, fecha_nacimiento, Toast.LENGTH_LONG).show()
+
+
+        }, year, month, day)
+        dpd.show()
+    }
+
+
+
+
 
     private fun validarCampos() {
         input_names.setOnEditorActionListener() { v, actionId, event ->
@@ -87,6 +120,7 @@ class PersonalDataActivity : AppCompatActivity() {
         if(input_names.text.toString() == ""){
             txt_names.error = getString(R.string.msj_error_nombre)
         }else{
+
             txt_names.error = null
         }
     }
@@ -99,18 +133,23 @@ class PersonalDataActivity : AppCompatActivity() {
         }
     }
 
-    //Metodo para mostrar el DataPicker
-    @RequiresApi(Build.VERSION_CODES.N)
-    fun clickDataPicker(view: View) {
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
+    private fun imprimirEnConsola(){
+        var txt_title = findViewById<TextView>(R.id.txt_personalInfo)
+        var titulo = txt_title.text.toString()
+        var nombres= input_names.text.toString()
+        val apellidos = input_lastnames.text.toString()
+        val selectedRadioButtonId: Int = rg_genero.checkedRadioButtonId
+        val selectedRadioButton: RadioButton = findViewById(selectedRadioButtonId)
+        val rta_genero: String = selectedRadioButton.text.toString()
+        val nivel_academico = spinner.selectedItem.toString()
 
-        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            // Display Selected date in Toast
-            Toast.makeText(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show()
-        }, year, month, day)
-        dpd.show()
+        var datos : String
+
+        datos = "...\n$titulo\n$nombres\n$apellidos\n$rta_genero\n$fecha_nacimiento\n$nivel_academico"
+
+
+        Log.i("Seguimiento", datos )
     }
+
+
 }
