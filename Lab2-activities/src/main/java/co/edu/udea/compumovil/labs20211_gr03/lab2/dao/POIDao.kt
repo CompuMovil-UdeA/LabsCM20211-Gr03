3ae47e591a8e42ce12524b235ffde84d8fa93b42
Doly Jimenez <doly.jimenez11@gmail.com>
2021-09-06 12:43:13 -0500

    Creacion de modelos user y POI con su respectivo DAO, esquema inicial de BD

---
+++ Lab2-activities/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab2/dao/POIDao.kt
@@ -0,0 +1,21 @@
+package co.edu.udea.compumovil.labs20211_gr03.lab2.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import co.edu.udea.compumovil.labs20211_gr03.lab2.models.POI
+
+@Dao
+interface POIDao {
+    @Insert
+    fun insert(request: POI)
+
+    @Query(value = "SELECT * from tabla_POI ORDER BY sitioid DESC")
+    fun getAll(): LiveData<List<POI>>
+
+    @Update
+    fun update(request: POI)
+
+    @Query(value = "DELETE from tabla_POI where sitioid = :key")
+    fun delete(key:Int)
+
+}
