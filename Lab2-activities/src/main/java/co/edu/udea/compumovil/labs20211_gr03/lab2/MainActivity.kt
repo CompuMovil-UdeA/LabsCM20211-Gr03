e1ea929a6a031eb4e1f491e3e41ed8342c34e162
ddiiego4 <dalberto.duque@udea.edu.co>
2021-09-06 02:05:08 -0500

    Creacion Login y Registro

--- Lab2-activities/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab2/MainActivity.kt
+++ Lab2-activities/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab2/MainActivity.kt
@@ -1,11 +1,22 @@
 package co.edu.udea.compumovil.labs20211_gr03.lab2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Lo envia al formulario de registro de usuario
+        btn = findViewById(R.id.registerBtn)
+        btn.setOnClickListener {
+            val intent = Intent(this, registro_usuario::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ Lab2-activities/src/main/java/co/edu/udea/compumovil/labs20211_gr03/lab2/registro_usuario.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.labs20211_gr03.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registro_usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro_usuario)
+    }
+}
